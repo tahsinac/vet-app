d412c1c67600727b474050abc2b1edd5c5e0c1e9
kfalez <kelten.falez@ucalgary.ca>
2021-11-17 20:55:24 -0700

    UserService for API

---
+++ vet-app/src/main/java/com/group213/vet/app/service/UserService.java
@@ -0,0 +1,34 @@
+package com.group213.vet.app.service;
+
+
+import com.group213.vet.app.model.User;
+import com.group213.vet.app.respository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> listAllUser(){
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+
+    public User getUser(Integer id){
+        return userRepository.findById(id).get();
+    }
+
+    public void deleteUser(Integer id){
+        userRepository.deleteById(id);
+    }
+}
