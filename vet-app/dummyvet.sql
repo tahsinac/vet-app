7327402f393b28fa5fa4d3996f82aeead4ab481e
kfalez <kelten.falez@ucalgary.ca>
2021-11-18 20:19:56 -0700

    Edited database to include all necessary attributes

--- vet-app/dummyvet.sql
+++ vet-app/dummyvet.sql
@@ -2,6 +2,24 @@ DROP DATABASE IF EXISTS DUMMYVET;
 CREATE DATABASE DUMMYVET; 
 USE DUMMYVET;
 
+CREATE TABLE Users (
+    id                  integer AUTO_INCREMENT not null,
+    username            VARCHAR(30),
+    theType             VARCHAR(30),
+    email               VARCHAR(30),
+    activationDate      VARCHAR(30),
+
+    
+    primary key (id)
+);
+
+INSERT INTO Users (id, username, theType, email, activationDate)
+VALUES
+('1','user1','admin', 'admin@ucalgary.ca', '2021-03-04'),
+('2','user2', 'animal technician', 'a.technician@ucalgary.ca', '2021-03-04'),
+('3','user3','teach tech', 'tt@ucalgary.ca', '2021-03-04'),
+('4','user4', 'animal technician', 'b.technician@ucalgary.ca', '2021-03-04');
+
 DROP TABLE IF EXISTS Animals;
 CREATE TABLE Animals (
     animalId           integer AUTO_INCREMENT not null,
@@ -20,33 +38,38 @@ CREATE TABLE Animals (
     subspecies          VARCHAR(30),
     distinguishingFeatures   VARCHAR(30),
     color               VARCHAR(30),
+    requestedBy			integer,
+	alerts			  VARCHAR(30),
     
     
-    primary key (animalId)
+    primary key (animalId),
+    foreign key (requestedBy) references Users(id)
 );
 
-INSERT INTO Animals (animalId, species, weight, tattooNum, cityTattoo, birthDate, breed, sex, rfid, microchip, theStatus, diet, region, subspecies, distinguishingFeatures, color)
+INSERT INTO Animals (animalId, species, weight, tattooNum, cityTattoo, birthDate, breed, sex, rfid, microchip, theStatus, diet, region, subspecies, distinguishingFeatures, color, requestedBy, alerts)
 VALUES
-('1', 'Dog', 3, 234234, 'HOC London', '12/07/2019', 'Beagle', 'MN', 176387613813, 20, 'Available', null, null, null, 'barks', 'Brown'),
-('2', 'Dog', 3, 234234, 'HOC Paris', '11/01/2018', 'Pitbull', 'MN', 30, 40, 'Available', null, null, null, "barks a lot", "White"),
-('3', 'Cow', 123, 981733, 'CBH India', '2018-02-29', 'Abigar', 'MN', 50, 60, 'Sick', 'Dairy', 'Spain', 'Taurus', null, null);
+('1', 'Dog', 3, 234234, 'HOC London', '12/07/2019', 'Beagle', 'MN', 176387613813, 20, 'Available', null, null, null, 'barks', 'Brown', null, null),
+('2', 'Dog', 3, 234234, 'HOC Paris', '11/01/2018', 'Pitbull', 'MN', 30, 40, 'Available', null, null, null, "barks a lot", "White", 2, 'needs attention'),
+('3', 'Cow', 123, 981733, 'CBH India', '2018-02-29', 'Abigar', 'MN', 50, 60, 'Sick', 'Dairy', 'Spain', 'Taurus', null, null, '2', 'needs attention');
 -- (1, 'test', 1, 1, 'test', 'test', 'test','test',10, 10, 'test', null, null, null, 'test', 'test');
 
 DROP TABLE IF EXISTS AnimalStatus;
 CREATE TABLE AnimalStatus (
+    statusId           integer AUTO_INCREMENT not null,
     animalId           integer,
     theDate            VARCHAR(30),
     theDescription     VARCHAR(30),
     location           VARCHAR(30),
     theStatus          VARCHAR(30),
-	
+
+    primary key (statusId),
     foreign key (animalId) references Animals(animalId)
 );
 
-INSERT INTO AnimalStatus (animalId, theDate, theDescription, location, theStatus)
+INSERT INTO AnimalStatus (statusId, animalId, theDate, theDescription, location, theStatus)
 VALUES
-('1','2021-12-12', null, 'In Campus', 'Available'),
-('2','2021-11-01', 'Her foot is injured', 'Hospital', 'Injured');
+('1', '1','2021-12-12', null, 'In Campus', 'Available'),
+('2', '2','2021-11-01', 'Her foot is injured', 'Hospital', 'Injured');
 
 DROP TABLE IF EXISTS AnimalStatusImages;
 CREATE TABLE AnimalStatusImages (
@@ -62,24 +85,6 @@ INSERT INTO AnimalStatusImages(imageId, animalId, statusHistory)
 VALUES
 (1, 2, 1),
 (2, 2, 2);
-
-CREATE TABLE Users (
-    id                  integer AUTO_INCREMENT not null,
-    username            VARCHAR(30),
-    theType             VARCHAR(30),
-    email               VARCHAR(30),
-    activationDate      VARCHAR(30),
-
-    
-    primary key (id)
-);
-
-INSERT INTO Users (id, username, theType, email, activationDate)
-VALUES
-('1','user1','admin', 'admin@ucalgary.ca', '2021-03-04'),
-('2','user2', 'animal technician', 'a.technician@ucalgary.ca', '2021-03-04'),
-('3','user3','teach tech', 'tt@ucalgary.ca', '2021-03-04'),
-('4','user4', 'animal technician', 'b.technician@ucalgary.ca', '2021-03-04');
 
 DROP TABLE IF EXISTS ExampleHistory;
 CREATE TABLE ExampleHistory (
@@ -102,7 +107,7 @@ VALUES
 
 DROP TABLE IF EXISTS Images;
 CREATE TABLE Images(
-    imageId            integer,
+    imageId            integer AUTO_INCREMENT not null,
     userId            integer,
     creationDate         VARCHAR(30),
     theFile                VARCHAR(30),
@@ -123,7 +128,7 @@ VALUES
 
 DROP TABLE IF EXISTS TheComment;
 CREATE TABLE TheComment (
-    commentId           integer not null,
+    commentId           integer AUTO_INCREMENT not null,
     userId              integer,
     animalId            integer,
     theDescription      varchar(30),
@@ -168,19 +173,20 @@ CREATE TABLE PrescriptionRecords(
     dosage             varchar(30),
     deliveryMethod     varchar(30),
     drugName           varchar(30),
-    treatmentMethodId  integer not null,
+--     treatmentMethodId  integer not null,
     
     primary key (scriptRecord),
     foreign key (userId) references Users(id),
-    foreign key (animalId) references Animals(animalId),
-    foreign key (treatmentMethodId) references TreatmentMethod(treatmentId)
+    foreign key (animalId) references Animals(animalId)
+    -- foreign key (treatmentMethodId) references TreatmentMethod(treatmentId)
 );
 
-INSERT INTO PrescriptionRecords(scriptRecord, userId, animalId, theDate, instructions, drugId, dosage, deliveryMethod, drugName, treatmentMethodId)
+INSERT INTO PrescriptionRecords(scriptRecord, userId, animalId, theDate, instructions, drugId, dosage, deliveryMethod, drugName)-- , treatmentMethodId)
 VALUES
-(1, 3, 1, "08/12/2020", null, null, null, null, null, 1),
-(2, 3, 2, "01/10/2021", null, null, null, null, null, 2),
-(3, 3, 2, "01/10/2021", null, null, null, null, null, 3);
+(1, 3, 1, "08/12/2020", null, null, null, null, null),-- , 1),
+(2, 3, 2, "01/10/2021", null, null, null, null, null),-- , 2),
+(3, 3, 2, "01/10/2021", null, null, null, null, null),
+(4, 3, 1, "01/10/2021", null, null, null, null, null);-- , 3);
  
 
 
